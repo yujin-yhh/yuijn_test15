d0e22a124b469126b11e21abc373b5b538efbeb1
yujin-yhh <1158501442@qq.com>
2021-04-18 15:13:12 +0800

    git配置s

--- pythoncodings/calculator.py
+++ pythoncodings/calculator.py
@@ -1,12 +1,15 @@
 class Calculator:
-    def add(self,a,b):
+    def add(self, a, b):
         return a + b
 
-    def sub(self,a,b):
+    def sub(self, a, b):
         return a - b
 
-    def mul(self,a,b):
+    def mul(self, a, b):
         return a * b
 
-    def div(self,a,b):
-        return a / b
+    def div(self, a, b):
+        if b == 0:
+            return 'ERROR'
+        else:
+            return a / b
--- testcase/test_calc.py
+++ testcase/test_calc.py
@@ -3,7 +3,16 @@ import pytest
 
 
 class TestCal:
+    def setup_class(self):
+        self.calc = Calculator()
+
     def test_add(self):
-        cal = Calculator()
-        result = cal.add(1, 2)
+        result = self.cal.add(1, 2)
         assert result == 3
+
+    @pytest.mark.parametrize("a,b,expectd", [
+        [1, 0, 'ERROR'], [4, 2, 2], [0.4, 0.1, 4]
+    ])
+    def test_div(self, a, b, expectd):
+        result = self.calc.div(a, b)
+        assert result == expectd
